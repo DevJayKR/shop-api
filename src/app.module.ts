06fd918a8cee606efd66d05a754af068974d4805
Junsung Choi <choiexe1@gmail.com>
2023-03-15 03:03:05 +0900

    configuration, docker setting

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        // DATABASE INFO
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+
+        // JWT INFO
+        JWT_ACCESS_SECRET_KEY: Joi.string().required(),
+        JWT_ACCESS_EXPIRATION_TIME: Joi.number().required(),
+        JWT_VERIFICATION_TOKEN_SECRET: Joi.string().required(),
+        JWT_VERIFICATION_TOKEN_EXPIRATION_TIME: Joi.number().required(),
+
+        // MAIL INFO
+        EMAIL_CONFIRMATION_URL: Joi.string().required(),
+        EMAIL_SERVICE: Joi.string().required(),
+        EMAIL_USER: Joi.string().required(),
+        EMAIL_PASSWORD: Joi.string().required(),
+
+        // SERVER INFO
+        SERVER_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
